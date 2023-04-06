033948cf91d3381a264363f874d4a693cddee9b0
Ghulam Subbhani <gsmalik030@gmail.com>
2023-04-06 17:07:40 +0200

    Fixed linters errors

--- main.rb
+++ main.rb
@@ -2,18 +2,14 @@ require_relative 'my_list'
 
 # Create our list
 list = MyList.new(1, 2, 3, 4)
-# <MyList: @list=[1, 2, 3, 4]>
 
 # Test #all?
-puts (list.all? { |e| e < 5 })
-puts (list.all? { |e| e > 5 })
-puts list.all? 
+puts(list.all? { |e| e < 5 })
+puts(list.all? { |e| e > 5 })
 
 # Test #any?
-puts (list.any? { |e| e == 2 })
-puts (list.any? { |e| e == 5 })
-puts list.any?
+puts(list.any? { |e| e == 2 })
+puts(list.any? { |e| e == 5 })
 
 # Test #filter
-puts (list.filter { |e| e.even? })
-puts list.filter
+puts(list.filter(&:even?))
